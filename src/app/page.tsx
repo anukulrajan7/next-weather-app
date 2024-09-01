b81236284eb4261686803b13ace63118cd19a000
Anukul Raj <anukulraj2004@gmail.com>
2024-09-01 19:45:56 +0530

    fix: Resolve 403 error for weather data fetch due to incorrect API usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import SearchBox from '@/components/autoSearch';
+import React, { Suspense } from 'react';
 import { store } from '../store/store';
 import { Provider } from 'react-redux';
 import WeatherPage from '@/components/weatherInfo';
@@ -7,8 +8,10 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between  my-7">
 			<Provider store={store}>
-				<SearchBox />
-				<WeatherPage />
+				<Suspense fallback={<div>Loading search box...</div>}>
+					<SearchBox />
+					<WeatherPage />
+				</Suspense>
 			</Provider>
 		</main>
 	);
